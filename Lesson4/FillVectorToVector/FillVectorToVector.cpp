4df0857ab9549e9ee14b700af9e197ce9ddc9642
DESKTOP-SM40OVD\sarov-win <uvsokolovsky@robotics.by>
2022-02-12 17:30:14 +0300

    Add Lesson4 files.

---
+++ Lesson4/FillVectorToVector/FillVectorToVector.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//Function for print vector
+void printVector(const std::vector<int>& values)
+{
+    for (int n : values)
+    {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    std::vector<int> v1{0, 1, 2, 3, 4, 5, 6, 7};
+    std::vector<int> v2;
+    
+    //v2.assign(v1.begin() + v1.size()/2, v1.end());
+    //copy(v1.begin() + v1.size()/2, v1.end(), back_inserter(v2));
+    for_each(v1.begin() + v1.size()/2, v1.end(), [&v2](const int n) {v2.push_back(n); });
+
+    std::cout << "First vector values: ";
+    printVector(v1);
+
+    std::cout << "Second vector values: ";
+    printVector(v2);
+        
+    return 0;
+}
+
---
+++ Lesson4/GreedyBackpack/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+#include "Object.h"
+
+int main()
+{
+    std::multiset <Object> objects; 
+    double backpackCapacity = 12.5;   //backpack Capacity
+        
+    objects.insert(Object(1.1, 1.1));
+    objects.insert(Object(2.2, 2.4));
+    objects.insert(Object(4.4, 4.1));
+    objects.insert(Object(3.3, 3.7));
+    objects.insert(Object(2.2, 2.4));
+    objects.insert(Object(5.5, 6.6));
+
+    //Print objects list
+    std::cout << "== Object list sort by value ==" << std::endl;
+    for (auto n : objects)
+    {
+        n.print();
+    }
+
+    //Put objects in backpack and print them
+    std::cout << "\n====  Objects in backpack  ====" << std::endl;
+    for (auto n : objects) 
+    {
+        if (n.getWeight() <= backpackCapacity)
+        {
+            backpackCapacity -= n.getWeight();
+            n.print();
+        }
+    }
+}
