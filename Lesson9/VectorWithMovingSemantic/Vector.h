a132ae4989e9e6702a85dd03284998f9e6f57143
DESKTOP-SM40OVD\sarov-win <uvsokolovsky@robotics.by>
2022-04-12 21:30:44 +0300

    Lesson 9 add.

---
+++ Lesson9/VectorWithMovingSemantic/Vector.h
@@ -0,0 +1,273 @@
+#pragma once
+#include <iostream>
+#include <ostream>
+#include <exception>
+
+template<class T>
+class Vector
+{
+	private:
+		int vSize;
+		int vCapacity;
+		T* elements;
+	public:
+		Vector();
+		Vector(int s);
+		Vector(Vector& other);
+		Vector(Vector&& other) noexcept;
+		~Vector();
+		int size();
+		int capacity();
+		bool empty();
+		void addMemory();
+		void push_back(const T& value);
+		void pop_back();
+		void insert(const int index, const T& value);
+		void remove(const int index);
+		void clear();
+		T* begin();
+		const T* begin() const;
+		T* end();
+		const T* end() const;
+		T& operator[](const int index);
+		const T& operator[](const int index) const;
+		Vector<T>& operator=(Vector<T>& other);
+		Vector<T>& operator=(Vector<T>&& other) noexcept;
+		friend std::ostream& operator<<(std::ostream& os, const Vector<T>& vec);
+};
+
+//Default constructor
+template<class T>
+Vector<T>::Vector() :vSize(0), elements(new T[4]), vCapacity(4)
+{
+	std::cout << "Default Vector constructor" << std::endl;
+}
+
+//Vector with capacity constructor 
+template<class T>
+Vector<T>::Vector(int s) : vSize(0), elements(new T[s]), vCapacity(s)
+{
+	std::cout << "Vector constructor, capacity = " << s << std::endl;
+}
+
+//Destructor
+template<class T>
+Vector<T>::~Vector()
+{
+	std::cout << "Vector destructor" << std::endl;
+	delete[] elements;
+}
+
+//Copy constructor
+template<class T>
+Vector<T>::Vector(Vector& other):elements(new T[other.vCapacity]), vSize(other.vSize), vCapacity(other.vCapacity)
+{
+	std::cout << "Copy constructor" << std::endl;
+
+    for (int i = 0; i < other.vSize; ++i)
+	{
+		elements[i] = other.elements[i];
+	}
+}
+
+//Moving constructor
+template<class T>
+Vector<T>::Vector(Vector<T>&& other) noexcept: elements(other.elements), vSize(other.vSize), vCapacity(other.vCapacity)
+{
+	std::cout << "Moving constructor" << std::endl;
+	other.elements = nullptr;
+	other.vSize = 0;
+	other.vCapacity = 0;
+}
+
+//Return vector size
+template<class T>
+int Vector<T>::size()
+{
+	return vSize;
+}
+
+//Return vector capacity
+template<class T>
+int Vector<T>::capacity()
+{
+	return vCapacity;
+}
+
+//Return is vector empty
+template<class T>
+bool Vector<T>::empty()
+{
+	return vSize == 0;
+}
+
+//Increase vector size method
+template<class T>
+void Vector<T>::addMemory()
+{
+	vCapacity *= 2;
+	T* tmp = elements;
+	elements = new T[vCapacity];
+
+	for (int i = 0; i < vSize; ++i) {
+		elements[i] = tmp[i];
+	}
+
+	delete[] tmp;
+}
+
+//Add new (last) element to vector
+template<class T>
+void Vector<T>::push_back(const T& value)
+{
+	if (vSize >= vCapacity) {
+		addMemory();
+	}
+
+	elements[vSize++] = value;
+}
+
+//Delete last element from vector
+template<class T>
+void Vector<T>::pop_back()
+{
+	if (vSize != 0) {
+		elements[vSize--] = 0;
+	}
+}
+
+//Insert element into vector
+template<class T>
+void Vector<T>::insert(const int index, const T& value)
+{
+	if (index > vSize - 1) {
+		throw std::out_of_range("Index out of range");
+	}
+
+	++vSize;
+
+	if (vSize >= vCapacity) {
+		addMemory();
+	}
+
+	for (int i = vSize; i > index; i--) {
+		elements[i] = elements[i - 1];
+	}
+
+	elements[index] = value;
+}
+
+
+template<class T>
+void Vector<T>::remove(int index)
+{
+	if (index > vSize - 1) {
+		throw std::out_of_range("Index out of range");
+	}
+
+	for (int i = index + 1; i < vSize; ++i) {
+		elements[i - 1] = elements[i];
+	}
+
+	--vSize;
+}
+
+template<class T>
+void Vector<T>::clear()
+{
+	for (int i = vSize - 1; i >= 0; i--)
+	{
+		elements[i] = 0;
+		--vSize;
+	}
+}
+
+//Iterator begin
+template<class T>
+T* Vector<T>::begin()
+{
+	return &elements[0];
+}
+
+//Constant iterator begin
+template<class T>
+const T* Vector<T>::begin() const
+{
+	return &elements[0];
+}
+
+//Iterator end
+template<class T>
+T* Vector<T>::end()
+{
+	return &elements[vSize];
+}
+
+//Constant iterator end
+template<class T>
+const T*  Vector<T>::end() const
+{
+	return &elements[vSize];
+}
+
+//Return element by index (operator [])
+template<class T>
+T& Vector<T>::operator[](int index)
+{
+	if (index > vSize - 1) {
+		throw std::out_of_range("Index out of range");
+	}
+
+	return elements[index];
+}
+
+//Return element by index (const operator[])
+template<class T>
+const T& Vector<T>::operator[](int index) const
+{
+	if (index > vSize - 1) {
+		throw std::out_of_range("Index out of range!!!");
+	}
+
+	return elements[index];
+}
+
+//Copy operator
+template<class T>
+Vector<T>& Vector<T>::operator=(Vector& other)
+{
+	std::cout << "Copy operator=()" << std::endl;
+
+	if (this != &other) {
+		Vector<T> tmp(other);
+		std::swap(elements, tmp.elements);
+		std::swap(vSize, tmp.vSize);
+		std::swap(vCapacity, tmp.vCapacity);
+	}
+
+	return *this;
+}
+
+//Copy operator with moving semntic
+template<class T>
+Vector<T>& Vector<T>::operator=(Vector&& other) noexcept
+{
+	std::cout << "Copy moving operator=()" << std::endl;
+
+	if (this != &other) {
+		std::swap(elements, other.elements);
+		std::swap(vSize, other.vSize);
+		std::swap(vCapacity, other.vCapacity);
+	}
+
+	return *this;
+}
+
+//Put vector to ostream
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Vector<T>& vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+		os << vec[i] << " ";
+	return os;
+}
---
+++ Lesson9/VectorWithMovingSemantic/main.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include "Vector.h"
+
+//Function for moving constructor check
+Vector<int> func()
+{
+    Vector<int> values(10);
+    return values;
+}
+
+int main()
+{
+    //Create new vector
+    Vector<int> values;
+
+    std::cout << "\n=============== Different vector methods calling ===============";
+    for (int i = 0; i < 5; i++) {
+        values.push_back(i+1);
+        std::cout << "\nPush_back(" << i+1 <<")";
+    }
+
+    //Check vector methods
+    std::cout << "\nVector size = " << values.size();
+    std::cout << "\nVector capacity = " << values.capacity();
+    std::cout << "\nVector values: ";
+
+    //Print vector values and check begin()/end() methods
+    for (auto v : values) {
+        std::cout << v << " ";
+    }
+
+    //Chech methods and operator[] 
+    std::cout << "\nRemove element " << values[3] << " in position 4";
+    values.remove(3);
+    std::cout << "\nPop_back(). Delete value " << values[values.size()-1];
+    values.pop_back();
+    std::cout << "\nInsert value 0 in position 0";
+    values.insert(0, 0);
+    std::cout << "\nSet value 10 in position 3";
+    values[3] = 10;
+
+    //Print vector values and check operator[]
+    std::cout << "\nVector values: ";
+    for (int i = 0; i < values.size(); i++) {
+        std::cout << values[i] << " ";
+    }
+
+    //Check clear() and empty() methods
+    values.clear();
+    std::cout << "\nClear vector";
+
+    if (values.empty())
+    {
+        std::cout << "\nVector values is empty" << std::endl;
+    }
+
+    //Different constructors and operators calling
+    std::cout << "\n================ Different constructors calling ================" << std::endl;
+
+    //Using copy constructor
+    Vector<int> valuesCopy(values);
+
+    //Using copy operators
+    Vector<int> valuesCopy2;
+    valuesCopy2 = valuesCopy;
+
+    //Using moving constructor
+    auto valuesCopy3 = func();
+
+    //Using copy moving operator
+    valuesCopy2 = std::move(values);
+
+    //std::cout << values << std::endl;
+    return 0;
+}
+
+ 
